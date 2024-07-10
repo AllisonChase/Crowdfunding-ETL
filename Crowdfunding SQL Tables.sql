43f537c3bf24c5b6b8c71a73f506fb277308f1d8
Allison Chase <aachase1401@gmail.com>
2024-07-10 18:47:59 -0400

    additonal SQL part of assignment

---
+++ Crowdfunding SQL Tables.sql
@@ -0,0 +1,60 @@
+CREATE TABLE "CATEGORY" (
+    "category_id" VARCHAR(100)   NOT NULL,
+    "category" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_CATEGORY" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "SUBCATEGORY" (
+    "subcategory_id" VARCHAR(100)   NOT NULL,
+    "sub-category" VARCHAR(100)   NULL,
+    CONSTRAINT "pk_SUBCATEGORY" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "CAMPAIGN" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(100)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "currency" VARCHAR(100)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(100)   NOT NULL,
+    "subcategory_id" VARCHAR(100)   NULL,
+    CONSTRAINT "pk_CAMPAIGN" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "CONTACTS" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_CONTACTS" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "CONTACTS" ("contact_id");
+
+ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_category_id" FOREIGN KEY("category_id")
+REFERENCES "CATEGORY" ("category_id");
+
+ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "SUBCATEGORY" ("subcategory_id");
+
+
+SELECT * FROM "CAMPAIGN";
+SELECT * FROM "CATEGORY";
+SELECT * FROM "CONTACTS";
+SELECT * FROM "SUBCATEGORY";
